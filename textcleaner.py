38a36a179e289dd523f107a3899730ad9327882a
Marcus D <marcusd.9234@gmail.com>
2019-12-01 03:49:27 -0600

    headers removed

--- textcleaner.py
+++ textcleaner.py
@@ -24,6 +24,7 @@ minFontSize = 9	#in px
 headerSearch = 5
 footerSearch = 5
 marginAllowed = 0.95	#margin allowed between header and footer match; this is really just used if page # is part of the footer
+minLength = 20	#number of characters that must match at the start of a sentence in order to be considered equivalent (for header)
 pageAllowed = 3	#number of pages that may fail to match header and footer before we consider the current match to not be a footer
 
 pageText = []
@@ -147,54 +148,98 @@ print(pageText)
 print()
 print()
 #remove headers and footers by comparng across pages
-#footers first
-footerList = []	#list of dicts, where each key in the dict is style+footer, and value is [count, footer, [pages]]
-for i in range(1, footerSearch + 1):
-	footerCount = {}
-	for n, p in enumerate(pageText):
-		try:
-			footer = str(list(p[-i].values())[0])
-			#handle very short strings, so is probably pg #: assumming page # < 1000
-			if len(footer) <= 3:
-				# print("short")
-				footerKey = "short"
-			else:
-				footerKey = str(p[-i])
-			if footerKey not in footerCount:
-				#try comparing if key is close to an existing key
-				keyList = footerCount.keys()
-				if len(keyList) == 0:	#nothing in dict so far
-					footerCount[footerKey] = [1, footer, [n]]
+def removeBorder(loc):
+	if loc not in ["footer", "header"]:
+		return []
+	#footers first
+	footerList = []	#list of dicts, where each key in the dict is style+footer, and value is [count, footer, [pages], directMatch]
+	for i in range(1, footerSearch + 1):
+		footerCount = {}
+		if loc == "footer":
+			i = -i
+		else:
+			i -= 1
+		for n, p in enumerate(pageText):
+			try:
+				footer = str(list(p[i].values())[0])
+				#handle very short strings, so is probably pg #: assumming page # < 1000
+				if len(footer) <= 3:
+					# print("short")
+					footerKey = "short"
 				else:
-					added = False
-					seqM = SequenceMatcher(a=footerKey)
-					for k in keyList:
-						if k == "short":
-							continue
-						seqM.set_seq2(k)
-						if seqM.quick_ratio() > marginAllowed:
-							footerCount[k][0] += 1
-							footerCount[k][2].append(n)
-							added = True
-							break
-					if not added:
-						#otherwise we need to just accept that it is probably not a footer
+					footerKey = str(p[i])
+				if footerKey not in footerCount:
+					#try comparing if key is close to an existing key
+					keyList = footerCount.keys()
+					if len(keyList) == 0:	#nothing in dict so far
 						footerCount[footerKey] = [1, footer, [n]]
-			else:
-				footerCount[footerKey][0] += 1
-				footerCount[footerKey][2].append(n)
-		except:
-			print("error")
-			continue
-	print(len(footerCount))
-	if len(footerCount) > pageAllowed:
-		break	#unlikely anything past this point is a footer
-	footerList.append(footerCount)
+					else:
+						added = False
+						seqM = SequenceMatcher(a=footerKey)
+						for k in keyList:
+							if k == "short":
+								continue
+							# if len(footerCount[k][1]) >= minLength and len(footer) >= minLength and footerCount[k][1][:minLength+1] == footer[:minLength+1]:
+							# 	print("same head")
+							# 	footerCount[""]
+							seqM.set_seq2(k)
+							# print(seqM.quick_ratio())
+							if seqM.quick_ratio() > marginAllowed:
+								footerCount[k][0] += 1
+								footerCount[k][2].append(n)
+								added = True
+								break
+
+						#try to match the start if header
+						if not added and loc == "header":
+							# print("trying match:", footer)
+							seqM.set_seq1(footer)
+							for k in keyList:
+								storedFooter = footerCount[k][1]
+								# print("comparing to:", storedFooter)
+								seqM.set_seq2(storedFooter)
+								matchInfo = seqM.get_matching_blocks()[0]
+								# print(matchInfo)
+								if matchInfo.a == 0 and matchInfo.b == 0 and matchInfo.size >= minLength:
+									# print("matched!")
+									longFooter = footer if len(footer) > len(storedFooter) else storedFooter
+									newFooter = longFooter[:matchInfo.size]
+									# print(newFooter)
+									footerCount[newFooter] = [
+										footerCount[k][0] + 1,
+										newFooter,
+										footerCount[k][2] + [n],
+										True
+									]
+									added = True
+									break
+							if added and k != newFooter:
+								# print("removing old:", k)
+								del footerCount[k]
+
+						if not added:
+							#otherwise we need to just accept that it is probably not a footer
+							footerCount[footerKey] = [1, footer, [n]]
+				else:
+					footerCount[footerKey][0] += 1
+					footerCount[footerKey][2].append(n)
+			except:
+				print("error")
+				continue
+		# print("----------------------------------")
+		# print(len(footerCount))
+		# print(footerCount)
+		# print()
+		if len(footerCount) > pageAllowed:
+			break	#unlikely anything past this point is a footer
+		footerList.append(footerCount)
+	return footerList
 # print(footerList)
+#remove footers first
+print("footers")
+footerList = removeBorder("footer")
 #now remove the footers
 for f in footerList:
-	# print(f)
-	# print()
 	#take the footer that occurs the most often
 	count = 0
 	maxFooter = None
@@ -202,12 +247,43 @@ for f in footerList:
 		if v[0] > count:
 			count = v[0]
 			maxFooter = v
-			maxKey = k
 	print(maxFooter)
 	print()
 	for p in maxFooter[2]:
 		del pageText[p][-1]
+
 print()
+print("headers")
+#now do headers
+headerList = removeBorder("header")
+currentFirst = 0
+for f in headerList:
+	#take the header that occurs the most often
+	count = 0
+	maxHeader = None
+	for k, v in f.items():
+		if v[0] > count:
+			count = v[0]
+			maxHeader = v
+	print(maxHeader)
+	print()
+	modified = False
+	for p in maxHeader[2]:
+		if len(maxHeader) == 3:
+			del pageText[p][currentFirst]
+		elif len(maxHeader) == 4:	#need to look at the first string and remove the header
+			# print(pageText[p][currentFirst])
+			for k in pageText[p][currentFirst]:
+				#should only be 1
+				if pageText[p][currentFirst][k] != maxHeader[1]:
+					pageText[p][currentFirst][k] = pageText[p][currentFirst][k][len(maxHeader[1]):].strip()
+			# print(pageText[p][currentFirst])
+			modified = True
+			# pageText[p][0] = pageText[p][0][len(maxHeader[1]):]
+	if modified:
+		currentFirst += 1
+
+print("--------------------------------------------")
 for p in pageText:
 	print(p)
 	print()
